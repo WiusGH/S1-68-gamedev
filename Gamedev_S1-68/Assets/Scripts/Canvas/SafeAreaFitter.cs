279a8e362fc93af85035f25621307726b5fe0433
deku0w0 <chancilba@gmail.com>
2025-10-22 11:55:49 -0500

    Andres[add]: safe area and card preview

---
+++ Gamedev_S1-68/Assets/Scripts/Canvas/SafeAreaFitter.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+[ExecuteAlways]
+[RequireComponent(typeof(RectTransform))]
+public class SafeAreaFitter : MonoBehaviour
+{
+    private RectTransform rectTransform;
+    private Rect lastSafeArea;
+
+    void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        ApplySafeArea();
+    }
+
+    void ApplySafeArea()
+    {
+        Rect safeArea = Screen.safeArea;
+        lastSafeArea = safeArea;
+
+        Vector2 anchorMin = safeArea.position;
+        Vector2 anchorMax = safeArea.position + safeArea.size;
+
+        anchorMin.x /= Screen.width;
+        anchorMin.y /= Screen.height;
+        anchorMax.x /= Screen.width;
+        anchorMax.y /= Screen.height;
+
+        rectTransform.anchorMin = anchorMin;
+        rectTransform.anchorMax = anchorMax;
+    }
+}
